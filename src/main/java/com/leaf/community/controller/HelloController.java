6209e2dfe8bf1bcfd618e1116395c8d5f96b90bf
tao <810146694@qq.com>
2019-11-19 11:23:51 +0800

    init repo

---
+++ src/main/java/com/leaf/community/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.leaf.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping("Hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
